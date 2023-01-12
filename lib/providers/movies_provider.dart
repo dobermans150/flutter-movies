cee7e688104810aaaef318123950f6413de8f07b
Christian Guevara <chrisdb150@gmail.com>
2023-01-11 20:57:28 -0400

    peticiones http y mapeo de peticiones

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies/models/models.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  final String _apikey = 'aca088ed0cd6da276a7e1d954151b30c';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
+
   MoviesProvider() {
     print('Movies Provider');
     getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key': _apikey,
+      'language': _language,
+      'page': '1',
+    });
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    print(nowPlayingResponse.results[0].title);
   }
 }
